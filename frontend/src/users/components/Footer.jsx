c94985c848a37ec804b50d8b41683f0db0e45662
PrithvirajVNair <prithvirajvnair.pvr@gmail.com>
2026-02-03 01:38:01 +0530

    UPDATE : Frontend Design Improvement

---
+++ frontend/src/users/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <p className='text-center'>&copy; To Do List - {new Date().getFullYear()} | All Rights Reserved &reg;</p>
+    </div>
+  )
+}
+
+export default Footer
--- frontend/src/users/components/Header.jsx
+++ frontend/src/users/components/Header.jsx
@@ -1,3 +1,5 @@
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -18,8 +20,8 @@ const Header = () => {
   return (
     <div className='bg-black/10 h-15 backdrop-blur-2xl fixed w-full z-99'>
         <div className='flex justify-between items-center h-full px-20'>
-            <a className='text-blue-400 text-2xl font-bold' href="#">ToDo</a>
-            <button onClick={handleLogout} className='bg-blue-400 border text-white py-1 px-2 rounded hover:bg-white hover:text-blue-400 cursor-pointer duration-200 active:scale-97'>Log Out</button>
+            <a className='text-blue-400 text-2xl font-bold' href="#">ToDL</a>
+            <button onClick={handleLogout} className='bg-blue-400 border text-white py-1 px-2 rounded hover:bg-blue-500 cursor-pointer duration-200 active:scale-97'><FontAwesomeIcon icon={faArrowRightFromBracket} className='me-1' />Log Out</button>
         </div>
     </div>
   )
